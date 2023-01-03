43e90ef4a220c2d548f1e45f225b15cd24ccca57
Msqoop <supercoder@supercoders-air.speedport.ip>
2023-01-03 21:06:17 +0100

    JS1

---
+++ assets/js/main.js
@@ -0,0 +1,54 @@
+
+
+ // AUFGABE E I N S - Z W E I
+
+// This is a single line JavaScript comment
+document.write("I have comments in my JavaScript code!");
+//document.write("You can't see this!");
+//-->
+
+document.write("I have multi-line comments!");
+/*document.write("You can't see this!");
+document.write("You can't see this!");
+document.write("You can't see this!");
+document.write("You can't see this!");
+document.write("You can't see this!");
+document.write("You can't see this!");
+document.write("You can't see this!");*/
+
+
+// AUFGABE Z W E I
+
+let helloWorld = "Hello World"
+console.log(helloWorld)
+
+let pi = 3.14
+console.log(pi)
+
+const burjDubai = "828m"
+console.log(burjDubai)
+
+let fullName = "Jan Schmidt"
+console.log(fullName)
+
+
+const eifelTower = "324m"
+console.log(eifelTower)
+
+
+const camelCase = "camelCase"
+console.log(camelCase)
+
+let kursStatus = "true"
+console.log(kursStatus)
+
+
+
+
+
+
+
+
+
+
+
